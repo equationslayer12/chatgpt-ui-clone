e8f7354030b5343d884d0f9e324fd69093e33ea0
equationslayer12 <roy0708usa@gmail.com>
2024-08-02 15:57:44 +0300

    added website structure

---
+++ src/components/icons.tsx
@@ -0,0 +1,33 @@
+export function ChevronDownIcon(props: any) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='16'
+      height='16'
+      fill='currentColor'
+      viewBox='0 0 16 16'
+      {...props}
+    >
+      <path
+        fill-rule='evenodd'
+        d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708'
+      />
+    </svg>
+  );
+}
+
+export function UploadIcon(props: any) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='16'
+      height='16'
+      fill='currentColor'
+      viewBox='0 0 16 16'
+      {...props}
+    >
+      <path d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5' />
+      <path d='M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z' />
+    </svg>
+  );
+}
